41c5b1a51f159ad134f7eb2436779f6034d09672
kpil7 <kapil@zoop.one>
2024-08-16 17:20:50 +0530

    calling source lambda from report lambda

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 exports.handler = async (event) => {
   let body;
   try {
@@ -9,27 +10,75 @@ exports.handler = async (event) => {
     };
   }
 
-  
+
+  const config = {
+    method: "get",
+    url: "https://zevb7for47.execute-api.eu-north-1.amazonaws.com/default/sourcelambda",
+    headers: {
+      "Content-Type": "application/json",
+    }
+  };
+
   const category = body.category;
 
   switch (category) {
     case "lite":
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Lite category" }),
-      };
+      try {
+        config.data = JSON.stringify({ category: "lite" })
+        const response = await axios(config)
+        console.log("Response from the lambda function", response.data)
+        const data = response.data
+        return {
+          statusCode: response.status,
+          body: JSON.stringify(data),
+        };
+      } catch (error) {
+        console.log("Error from the lambda function", error)
+        return {
+          statusCode: error.response.status,
+          body: JSON.stringify(error.response.data),
+        };
+      }
+      break;
+
 
     case "advance":
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Advanced category" }),
-      };
+      try {
+        config.data = JSON.stringify({ category: "advance" })
+        const response = await axios(config)
+        console.log("Response from the lambda function", response.data)
+        const data = response.data
+        return {
+          statusCode: response.status,
+          body: JSON.stringify(data),
+        };
+      } catch (error) {
+        console.log("Error from the lambda function", error)
+        return {
+          statusCode: error.response.status,
+          body: JSON.stringify(error.response.data),
+        };
+      }
+      break;
 
     case "dynamic":
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Dynamic category" }),
-      };
+      try {
+        config.data = JSON.stringify({ category: "dynamic" })
+        const response = await axios(config)
+        console.log("Response from the lambda function", response.data)
+        const data = response.data
+        return {
+          statusCode: response.status,
+          body: JSON.stringify(data),
+        };
+      } catch (error) {
+        console.log("Error from the lambda function", error)
+        return {
+          statusCode: error.response.status,
+          body: JSON.stringify(error.response.data),
+        };
+      }
+      break;
 
     default:
       return {
